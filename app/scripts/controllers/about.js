0c3ccae8aeba366422b500e2624934dd7aa51a99
HugoRLopes <ramoslopes.hugo@gmail.com>
2016-02-01 16:36:36 +0100

    new project without compass

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sepamailFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sepamailFrontApp
+ */
+angular.module('sepamailFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
