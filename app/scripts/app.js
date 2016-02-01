e713ab2ca8636f3eb2e31171c3c3155d39a5fd32
HugoRLopes <ramoslopes.hugo@gmail.com>
2016-02-01 14:24:21 +0100

    reset project with Bootstrap integration

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sepamailFrontApp
+ * @description
+ * # sepamailFrontApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sepamailFrontApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sepamailFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sepamailFrontApp
+ */
+angular.module('sepamailFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
