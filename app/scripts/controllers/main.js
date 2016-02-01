300dbce326433867d88d829e927469c3535a1bf1
HugoRLopes <ramoslopes.hugo@gmail.com>
2016-02-01 12:19:29 +0100

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sepaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sepaApp
+ */
+angular.module('sepaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
