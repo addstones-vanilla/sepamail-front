ee5e39ad5a86f87c4a37e26c0f4b956ec46ef806
Erbstein <erbsteinni@eisti.eu>
2016-02-02 10:56:50 +0100

    ajout des routes

---
+++ app/scripts/controllers/listeanomalies.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sepamailFrontApp.controller:ListeanomaliesCtrl
+ * @description
+ * # ListeanomaliesCtrl
+ * Controller of the sepamailFrontApp
+ */
+angular.module('sepamailFrontApp')
+  .controller('ListeanomaliesCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/recapmandat.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sepamailFrontApp.controller:RecapmandatCtrl
+ * @description
+ * # RecapmandatCtrl
+ * Controller of the sepamailFrontApp
+ */
+angular.module('sepamailFrontApp')
+  .controller('RecapmandatCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
