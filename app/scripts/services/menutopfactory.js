ac77a21a232324e50d32bab93b39b9cf2ec507f9
Erbstein <erbsteinni@eisti.eu>
2016-02-02 16:15:35 +0100

    meilleurs id pour les menus

--- app/scripts/services/menutopfactory.js
+++ app/scripts/services/menutopfactory.js
@@ -59,7 +59,7 @@ angular.module('sepamailFrontApp')
               "<nav class='navbar navbar-default' style='background-color:" + headerFactory.colorMenu + "'>" +
               "<div class='container-fluid'>" +
               "<div class='navbar-header'>" +
-              "<button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#arrive-navbar-collapse-1' aria-expanded='false'>"+
+              "<button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#depart-navbar-collapse-1' aria-expanded='false'>"+
               "<span class='sr-only'>Toggle navigation</span>"+
               "<span class='icon-bar'></span>"+
               "<span class='icon-bar'></span>"+
@@ -67,7 +67,7 @@ angular.module('sepamailFrontApp')
               "</button>"+
               "<a class='navbar-brand' href='/#/" + bankName + "/depart/accueil'>Accueil</a>"+
               "</div>" +
-              "<div class='collapse navbar-collapse' id='arrive-navbar-collapse-1'>"+
+              "<div class='collapse navbar-collapse' id='depart-navbar-collapse-1'>"+
               "<ul class='nav navbar-nav'>"+
               "<li>" +
               "<a href='/#/" + bankName + "/depart/listeOperations'>Changement de domiciliation</a>" +
@@ -89,7 +89,7 @@ angular.module('sepamailFrontApp')
               "<nav class='navbar navbar-default' style='background-color:" + headerFactory.colorMenu + "'>" +
               "<div class='container-fluid'>" +
               "<div class='navbar-header'>" +
-              "<button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#arrive-navbar-collapse-1' aria-expanded='false'>"+
+              "<button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#emetteur-navbar-collapse-1' aria-expanded='false'>"+
               "<span class='sr-only'>Toggle navigation</span>"+
               "<span class='icon-bar'></span>"+
               "<span class='icon-bar'></span>"+
@@ -97,7 +97,7 @@ angular.module('sepamailFrontApp')
               "</button>"+
               "<a class='navbar-brand' href='/#/" + bankName + "/emetteur/accueil'>Accueil</a>"+
               "</div>" +
-              "<div class='collapse navbar-collapse' id='arrive-navbar-collapse-1'>"+
+              "<div class='collapse navbar-collapse' id='emetteur-navbar-collapse-1'>"+
               "<ul class='nav navbar-nav'>"+
               "<li>" +
               "<a href='/#/" + bankName + "/emetteur/listeMandats'>Mandats de mobilité</a>" +
