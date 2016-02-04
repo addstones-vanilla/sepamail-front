37e67e9c96f7d973db9ba384367ca054a77d3755
Erbstein <erbsteinni@eisti.eu>
2016-02-04 13:31:53 +0100

    modification de la partie operation

--- app/scripts/controllers/listeanomalies.js
+++ app/scripts/controllers/listeanomalies.js
@@ -10,6 +10,21 @@
 angular.module('sepamailFrontApp')
   .controller('ListeanomaliesCtrl', function ($scope,$routeParams) {
     $scope.banque = $routeParams.banque_name;
+    $scope.etat = $routeParams.etat;
+
+    switch($scope.etat){
+      case "arrive":
+            $scope.filEtat = "Banque d'arrivé";
+            break;
+      case "depart":
+            $scope.filEtat = "Banque de départ";
+            break;
+      case "emetteur":
+            $scope.filEtat = "Banque d'emetteur"
+            break;
+      default:
+            $scope.filEtat = "erreur";
+    }
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
