c8bc9d8bd4e7f79206a6fef1fb4bf134fdf770f6
HugoRLopes <ramoslopes.hugo@gmail.com>
2016-02-05 10:13:10 +0100

    ajout D3js + changements formulaire + corrections tabs

--- app/scripts/controllers/arrive.js
+++ app/scripts/controllers/arrive.js
@@ -8,38 +8,59 @@
  * Controller of the sepamailFrontApp
  */
 angular.module('sepamailFrontApp')
-  .controller('ArriveCtrl', function (mandatsFactory,$scope) {
+  .controller('ArriveCtrl', function (mandatsFactory, $scope) {
 
+    $scope.labels = ['Jan 2017', 'Feb 2017', 'Mars 2017', 'Avril 2017', 'Mai 2017', 'Juin 2017'];
+    $scope.series = ['AGRIFRPPXXX', 'SOGEFRPPXXX', 'CEPAFRPPXXX', 'BNPAFRPPXXX'];
 
-    var mandats = mandatsFactory;
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55],
+      [28, 48, 40, 19, 86, 27],
+      [28, 48, 40, 19, 86, 27],
+      [28, 48, 40, 19, 86, 27]
+    ];
 
-    var newData = [];
+    $scope.labels2 = ["AGRIFRPPXXX", "SOGEFRPPXXX", "CEPAFRPPXXX, BNPAFRPPXXX"];
+    $scope.data2 = [25, 25, 25, 25];
 
+    var colorArray = ['#000000', '#660000', '#CC0000', '#FF6666'];
+    $scope.colorFunction = function () {
+      return function (d, i) {
+        return colorArray[i];
+      };
+    };
 
-    for(var index in mandats){
-      var frenchDate = mandats[index].SignedDate;
+    $scope.xFunction = function () {
+      return function (d) {
+        return d.key;
+      };
+    }
 
-      var ukDate = frenchDate.substring(6, 10) + "-" +  frenchDate.substring(3, 5);
+    $scope.yFunction = function () {
+      return function (d) {
+        return d.y;
+      };
+    }
 
-      var exist = false;
-
-      for(var index2 in newData){
-        if(newData[index2].date === ukDate){
-          newData[index2].trendingValue ++;
-
-          exist = true;
-        }
-      }
-
-      if(!exist){
-        newData.push(
-          { 'date' : ukDate,
-            'trendingValue' : 1
-          }
-        )
+    $scope.toolTipContentFunction = function () {
+      return function (key, x, y, e, graph) {
+        return '<div>' + key  + '</div>';
       }
     }
 
-    $scope.data = newData;
+
+    $scope.exampleData = [
+      {key: "AGRIFRPPXXX", y: 65},
+      {key: "SOGEFRPPXXX", y: 59},
+      {key: "CEPAFRPPXXX", y: 40},
+      {key: "BNPAFRPPXXX", y: 19}
+    ];
+
+
+    //var mandats = mandatsFactory;
+    //
+    //var newData = [];
+    //
+    //$scope.data = newData;
 
   });
